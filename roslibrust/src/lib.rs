cb0c38a29d7e2ccc2ce1b12fc719480c41a7db1f
carter <carter@amprobotics.com>
2025-01-07 14:24:59 -0700

    Fix path that is wild...

--- roslibrust/src/lib.rs
+++ roslibrust/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../README.md")]
+#![doc = include_str!("../../README.md")]
 
 // Re-export common types and traits under the roslibrust namespace
 pub use roslibrust_common::*;
